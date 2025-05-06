9b0dddd03645ebe71206c7601234c1588321f9af
Lucas Desport <desport@mit.edu>
2025-05-06 17:40:49 -0400

    turning the two graphs into functions

--- global_GHG_price.py
+++ global_GHG_price.py
@@ -13,57 +13,52 @@
 #     name: python3
 # ---
 
+# # Emissions price
+
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
-# # Emissions price
 
-# +
-with open("dfs.pkl", "rb") as f:
-    dfs = pickle.load(f)
+def emis_price(emis_type: str): #emis_type can only be 'co2' or 'ghg'
 
-dfd = dfs['data']
-# -
+    with open("dfd.pkl", "rb") as f:
+        df = pickle.load(f)
 
-# Pre-treatment to create a subcategory of the dataframe including only the parameter 'data' 
-dfd = dfs['data']
-dfd.columns = ['Attribute', 'Year', 'Region', 'Value', 'Scenario']
-dfd['Year'] = pd.to_numeric(dfd['Year'], errors='coerce')
-dfd = dfd[dfd['Year'] <= 2100]
+    if emis_type == 'co2':
+        df = df[df['Attribute'].isin(['46_CO2 price (US$ per ton CO2)'])]
+    elif emis_type == 'ghg':
+        df = df[df['Attribute'].isin(['46a_CO2eq price (US$ per ton CO2eq)'])]
+    else:
+        print("Error: choose emis_type = 'co2' or 'ghg'")
+        return None
 
-# Select attributes
-df_emis = dfd[dfd['Attribute'].isin(['46_CO2 price (US$ per ton CO2)',
-                                     '46a_CO2eq price (US$ per ton CO2eq)',
-                                    ])]
-df_pghg = dfd[dfd['Attribute'].isin(['46_CO2 price (US$ per ton CO2)',
-                                    ])]
+    pv = df.pivot_table(index=['Year'], columns='Scenario', values='Value', aggfunc='mean')
+    pv = pv.reset_index()
+    
+    # Plot
+    ax = pv.drop(columns=['Year']).plot(
+        kind='line', stacked=False, figsize=(14, 6), colormap='tab20')
+    
+    x_labels = pv['Year'].to_list()
+    ax.set_xticks(range(len(x_labels)))
+    ax.set_xticklabels(x_labels, rotation=90)
+    
+    # Labels, legend, etc.
+    plt.title("Global emissions price", weight='bold')
+    ax.set_ylabel("Price [$/tCO₂]")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='upper left', bbox_to_anchor=(1.05, 1))
+    ax.yaxis.grid(True, linestyle='--', alpha=0.5)
+    plt.gca().yaxis.set_major_locator(MultipleLocator(25))
+    plt.tight_layout()
+    
+    plt.savefig("global_emission_price.png", dpi=300, bbox_inches='tight')
+    
+    plt.show()
+        
+    return df
 
-# +
-pv_pghg = df_pghg.pivot_table(index=['Year'], columns='Scenario', values='Value', aggfunc='mean')
-pv_pghg = pv_pghg.reset_index()
-
-# Plot
-ax = pv_pghg.drop(columns=['Year']).plot(
-    kind='line', stacked=False, figsize=(14, 6), colormap='tab20')
-
-x_labels = pv_pghg['Year'].to_list()
-ax.set_xticks(range(len(x_labels)))
-ax.set_xticklabels(x_labels, rotation=90)
-
-# Labels, legend, etc.
-plt.title("Global average price of CO2", weight='bold')
-ax.set_ylabel("Price [$/tCO₂]")
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[::-1], labels[::-1], loc='upper left', bbox_to_anchor=(1.05, 1))
-ax.yaxis.grid(True, linestyle='--', alpha=0.5)
-plt.gca().yaxis.set_major_locator(MultipleLocator(25))
-plt.tight_layout()
-
-plt.savefig("global_emission_price.png", dpi=300, bbox_inches='tight')
-
-plt.show()
-# -
 
 
--- global_emissions_profile.py
+++ global_emissions_profile.py
@@ -13,107 +13,99 @@
 #     name: python3
 # ---
 
+# # Global emissions profile
+
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
+from pathlib import Path
 
-# # Global emissions profile
 
-# +
-with open("dfs.pkl", "rb") as f:
-    dfs = pickle.load(f)
+def gemis():
 
-dfd = dfs['data']
-# -
+    with open("dfd.pkl", "rb") as f:
+        df = pickle.load(f)
 
-# Pre-treatment to create a subcategory of the dataframe including only the parameter 'data' 
-dfd = dfs['data']
-dfd.columns = ['Attribute', 'Year', 'Region', 'Value', 'Scenario']
-dfd['Year'] = pd.to_numeric(dfd['Year'], errors='coerce')
-dfd = dfd[dfd['Year'] <= 2100]
-
-# Select attributes and convert MtCO2eq into GtCO2eq
-df_ghg = dfd[dfd['Attribute'].isin(['14a_GHGinCO2eq (million ton)',
+    # Select emissions variables
+    df = df[df['Attribute'].isin(['14a_GHGinCO2eq (million ton)',
                                     '06a_Pos_CO2_fossil (million ton)',
                                     '07_CO2_industrial (million ton)',
                                     '08_CO2_land use change (million ton)',
                                     '08b_NE_bioccs',
                                     '08c_NE_daccs'])]
-df_ghg.loc[:, 'Value'] = df_ghg['Value'] / 1000
+    df.loc[:, 'Value'] = df['Value'] / 1000 # to convert in GtCO2eq
 
-# +
-# Pivot and rename
-pv_ghg = df_ghg.pivot_table(index=['Scenario', 'Year'], columns='Attribute', values='Value', aggfunc='sum', sort=False)
-pv_ghg.columns = ['Fossil CO₂', 'Process CO₂', 'AFOLU CO₂', 'CDR - BECCS', 'CDR - DACCS', 'Non-CO₂ emissions']
-pv_ghg = pv_ghg.reset_index()
+    # Pivot and rename
+    pv = df.pivot_table(index=['Scenario', 'Year'], columns='Attribute', values='Value', aggfunc='sum', sort=False) # keeps scenarios order as listed in scenario_map
+    pv.columns = ['Fossil CO₂', 'Process CO₂', 'AFOLU CO₂', 'CDR - BECCS', 'CDR - DACCS', 'Non-CO₂ emissions'] # rename emissions variables
+    pv = pv.reset_index()
 
-# Colors
-colors = [
+    # set colors
+    colors = [
     '#d73027',  # Fossil CO₂
     '#f46d43',  # Process CO₂
     '#fdae61',  # AFOLU CO₂
     '#4575b4',  # CDR - BECCS
     '#74add1',  # CDR - DACCS
     '#66c2a5'   # Non-CO₂ emissions
-]
+    ]
 
-# Keep scenarios and years
-scenarios = pv_ghg['Scenario'].unique()
-years = sorted(pv_ghg['Year'].unique())
+    # Extract scenarios' names and years
+    scenarios = pv['Scenario'].unique()
+    years = sorted(pv['Year'].unique())
 
-# Create a single string index for plotting
-pv_ghg['Index'] = pv_ghg['Scenario'] + '-' + pv_ghg['Year'].astype(str)
-pv_ghg = pv_ghg.set_index('Index')
+    # Create a single string index for plotting
+    pv['Index'] = pv['Scenario'] + '-' + pv['Year'].astype(str)
+    pv = pv.set_index('Index')
 
-# Plot
-ax = pv_ghg.drop(columns=['Scenario', 'Year']).plot(
-    kind='bar', stacked=True, figsize=(14, 6), color=colors
-)
+    # Plot
+    ax = pv.drop(columns=['Scenario', 'Year']).plot(kind='bar', stacked=True, figsize=(14, 6), color=colors)
 
-# Set xticks to year only
-x_labels = pv_ghg.index.to_list()
-year_labels = [label.split('-')[1] for label in x_labels]
-ax.set_xticks(range(len(year_labels)))
-ax.set_xticklabels(year_labels, rotation=90)
+    # Set xticks to year only (first axis)
+    x_labels = pv.index.to_list()
+    year_labels = [label.split('-')[1] for label in x_labels]
+    ax.set_xticks(range(len(year_labels)))
+    ax.set_xticklabels(year_labels, rotation=90)
 
-# Second x-axis for scenarios
-ax2 = ax.twiny()
-ax2.set_xlim(ax.get_xlim())
+    # Second x-axis for scenarios (sercond axis)
+    ax2 = ax.twiny()
+    ax2.set_xlim(ax.get_xlim())
 
-# Calculate midpoint index of each scenario based on exact match in the Scenario column
-scenario_ranges = []
-for scenario in scenarios:
-    indices = pv_ghg[pv_ghg['Scenario'] == scenario].index
-    if len(indices) > 0:
-        midpoint = indices.tolist()
-        midpoint_pos = sum([pv_ghg.index.get_loc(i) for i in midpoint]) / len(midpoint)
-        scenario_ranges.append((midpoint_pos, scenario))
+    # Calculate midpoint index of each scenario based on exact match in the Scenario column
+    scenario_ranges = []
+    for scenario in scenarios:
+        indices = pv[pv['Scenario'] == scenario].index
+        if len(indices) > 0:
+            midpoint = indices.tolist()
+            midpoint_pos = sum([pv.index.get_loc(i) for i in midpoint]) / len(midpoint)
+            scenario_ranges.append((midpoint_pos, scenario))
 
-scenario_positions, scenario_labels = zip(*scenario_ranges)
-ax2.set_xticks(scenario_positions)
-ax2.set_xticklabels(scenario_labels)
-ax2.tick_params(axis='x', which='both', length=0)
-ax2.spines['top'].set_visible(False)
-ax2.set_frame_on(False)
-ax2.xaxis.set_label_position('bottom')
-ax2.xaxis.set_ticks_position('bottom')
-ax2.spines['bottom'].set_position(('outward', 40))
-ax.set_xlabel("")  # This removes the label "Index"
-ax.yaxis.grid(True, linestyle='--', alpha=0.5)
-plt.gca().yaxis.set_major_locator(MultipleLocator(5))
+    # Plot characteristics
+    scenario_positions, scenario_labels = zip(*scenario_ranges)
+    ax2.set_xticks(scenario_positions)
+    ax2.set_xticklabels(scenario_labels)
+    ax2.tick_params(axis='x', which='both', length=0)
+    ax2.spines['top'].set_visible(False)
+    ax2.set_frame_on(False)
+    ax2.xaxis.set_label_position('bottom')
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 40))
+    ax.set_xlabel("")  # This removes the label "Index"
+    ax.yaxis.grid(True, linestyle='--', alpha=0.5)
+    plt.gca().yaxis.set_major_locator(MultipleLocator(5)) # increment each 5 GtCO2eq
 
-# Labels, legend, etc.
-plt.title("Global emissions profile", weight='bold')
-ax.set_ylabel("Emissions [GtCO₂eq]")
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[::-1], labels[::-1], loc='upper left', bbox_to_anchor=(1.005, 1))
-plt.tight_layout()
+    # Labels, legend, etc.
+    plt.title("Global emissions profile", weight='bold')
+    ax.set_ylabel("Emissions [GtCO₂eq]")
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='upper left', bbox_to_anchor=(1.005, 1))
+    plt.tight_layout()
+    
+    plt.savefig(Path("global_emission_profile.png"), dpi=300, bbox_inches='tight')
+    
+    plt.show()
 
-plt.savefig("global_emission_profile.png", dpi=300, bbox_inches='tight')
-
-plt.show()
-
-# -
+    #return fig, ax
 
 
