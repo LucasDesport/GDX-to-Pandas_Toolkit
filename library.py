a059087adc251451ce5a6b6e591a076bcd05e4cc
Lucas Desport <desport@mit.edu>
2025-05-13 17:39:36 -0400

    introducing a file collecting dictionaries and dataframes such as nomenclatures

---
+++ library.py
@@ -0,0 +1,42 @@
+''' library '''
+
+import pandas as pd
+
+lib = pd.DataFrame({
+    'agy': {
+        'Yaxis': 'Production',
+        'EPPA_units': '10B USD',
+        'Converter': 1.0,
+        'Unit': 'billion USD'
+    },
+    'sco2': {
+        'Yaxis': 'Emissions',
+        'EPPA_units': '100 MtC',
+        'Converter': 44 / 12 * 100,
+        'Unit': 'MtCO2'
+    },
+    'DPD': {
+        'Yaxis': 'Domestic price',
+        'EPPA_units': '(relative value)',
+        'Converter': 1.0,
+        'Unit': '(relative value)'
+    },
+    'APA': {
+        'Yaxis': 'Armington price',
+        'EPPA_units': '(relative value)',
+        'Converter': 1.0,
+        'Unit': '(relative value)'
+    },
+    'imflow': {
+        'Yaxis': 'Imports',
+        'EPPA_units': '10B USD',
+        'Converter': 1.0,
+        'Unit': 'billion USD'
+    },
+    'exflow': {
+        'Yaxis': 'Exports',
+        'EPPA_units': '10B USD',
+        'Converter': 1.0,
+        'Unit': 'billion USD'
+    }
+}).T
