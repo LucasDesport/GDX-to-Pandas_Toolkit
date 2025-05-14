642326e514dcb5db0d46f64ecfc17143ac4d1aad
Lucas Desport <desport@mit.edu>
2025-05-14 14:30:15 -0400

    config.py no longer useful

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-scenario_map = {'bsin_fj and cj(fele) as in EPPADF': 'C:\\Users\\desport\\modeling\\EPPA7\\results\\all_NZ70-EPPA-DF.gdx',
-                'only cj(fele) from EPPADAC': 'C:\\Users\\desport\\modeling\\EPPA7\\results\\all_NZ70_cj(fele).gdx',
-                'only bsin_fj from EPPADAC': 'C:\\Users\\desport\\modeling\\EPPA7\\results\\all_NZ70_bsin_fj.gdx',
-                'bsin_fj and cj(fele) from EPPADAC': 'C:\\Users\\desport\\modeling\\EPPA7\\results\\all_NZ70_both.gdx',
-}
