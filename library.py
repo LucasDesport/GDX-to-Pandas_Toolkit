fbf9835f2553887334e39fca4ffd10a619e985fc
Lucas Desport <desport@mit.edu>
2025-05-16 14:08:15 -0400

    includes the parameter type to process specific plot functions

--- library.py
+++ library.py
@@ -7,36 +7,42 @@ lib = pd.DataFrame({
         'Yaxis': 'Production',
         'EPPA_units': '10B USD',
         'Converter': 1.0,
-        'Unit': 'billion USD'
+        'Unit': 'billion USD',
+        'type': 'monetary'
     },
     'sco2': {
         'Yaxis': 'Emissions',
         'EPPA_units': 'MtCO2',
         'Converter': 1.0,
-        'Unit': 'MtCO2'
+        'Unit': 'MtCO2',
+        'type': 'emission'
     },
     'DPD': {
         'Yaxis': 'Domestic price',
         'EPPA_units': '(relative value)',
         'Converter': 1.0,
-        'Unit': '(relative value)'
+        'Unit': '(relative value)',
+        'type': 'price'
     },
     'APA': {
         'Yaxis': 'Armington price',
         'EPPA_units': '(relative value)',
         'Converter': 1.0,
-        'Unit': '(relative value)'
+        'Unit': '(relative value)',
+        'type': 'price'
     },
     'imflow': {
         'Yaxis': 'Imports',
         'EPPA_units': 'B USD',
         'Converter': 1.0,
-        'Unit': 'billion USD'
+        'Unit': 'billion USD',
+        'type': 'monetary'
     },
     'exflow': {
         'Yaxis': 'Exports',
         'EPPA_units': 'B USD',
         'Converter': 1.0,
-        'Unit': 'billion USD'
+        'Unit': 'billion USD',
+        'type': 'monetary'
     }
 }).T
